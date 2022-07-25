87e7e234c1daaf757a723f52093d67ab1bf42799
Muhammad Abid <muhammad.abid.teo@gotoko.co.id>
2022-07-25 17:49:48 +0500

    JSON and XML Work

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IndexHandler(c *gin.Context) {
+	c.XML(200, Person{FirstName: "Muhammad", LastName: "Abid"})
+
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.GET("/", IndexHandler)
 	router.Run()
 }
