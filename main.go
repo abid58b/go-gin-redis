afa5097804972d0321952ebfd749f6e7e1e851ad
Muhammad Abid <muhammad.abid.teo@gotoko.co.id>
2022-07-26 14:01:08 +0500

    boilerplate

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"encoding/xml"
-
 	"github.com/gin-gonic/gin"
 )
 
-func IndexHandler(c *gin.Context) {
-	c.XML(200, Person{FirstName: "Muhammad", LastName: "Abid"})
-
-}
-
-type Person struct {
-	XMLName   xml.Name `xml:"person"`
-	FirstName string   `xml:"firstName,attr"`
-	LastName  string   `xml:"lastName,attr"`
-}
-
 func main() {
-	router := gin.Default()
-	router.GET("/", IndexHandler)
-	router.Run()
+	Router := gin.Default()
+	Router.Run()
 }
